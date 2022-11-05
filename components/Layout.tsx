268cef995af9a9aaa9852d89175e4d5b9a458c55
Mark Ambrocio <wowmarkambro@gmail.com>
2022-11-05 01:55:50 -0600

    add header font

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,6 +11,12 @@ export function Layout({ pageTitle = "Fresh", children }: Props) {
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,600;1,300;1,600&display=swap"
+          rel="stylesheet"
+        />
         <title>{pageTitle} | Dang that&apos;s Deno!</title>
       </Head>
       <main>
--- twind.config.ts
+++ twind.config.ts
@@ -2,6 +2,34 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  preflight: (preflight) => ({
+    ...preflight,
+    h1: {
+      fontFamily: "'Poppins', sans-serif",
+      textTransform: "uppercase",
+      fontWeight: "600 !important",
+    },
+    h2: {
+      fontFamily: "'Poppins', sans-serif",
+      textTransform: "uppercase",
+      fontWeight: "600 !important",
+    },
+    h3: {
+      fontFamily: "'Poppins', sans-serif",
+      textTransform: "uppercase",
+      fontWeight: "600 !important",
+    },
+    h4: {
+      fontFamily: "'Poppins', sans-serif",
+      textTransform: "uppercase",
+      fontWeight: "600 !important",
+    },
+    h5: {
+      fontFamily: "'Poppins', sans-serif",
+      textTransform: "uppercase",
+      fontWeight: "600 !important",
+    },
+  }),
   theme: {
     extend: {
       colors: {
@@ -12,6 +40,8 @@ export default {
       backgroundImage: {
         linear:
           "linear-gradient(90deg, rgba(72,222,212,1) 0%, rgba(160,38,191,1) 20%, rgba(232,44,117,1) 60%, rgba(245,255,0,1) 85%, rgba(72,222,212,1) 96%)",
+        storeCardTitle:
+          "linear-gradient(90deg, rgba(255,196,14,0.8) 50%, rgba(255,196,14,0.8) 100%)",
       },
     },
   },
