bb37b4dbb0ee650fd0b0aa3300eab9f6ce6fec54
Mark Ambrocio <wowmarkambro@gmail.com>
2022-11-05 02:21:33 -0600

    fix store card

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Head } from "https://deno.land/x/fresh@1.1.2/runtime.ts";
+import { asset, Head } from "https://deno.land/x/fresh@1.1.2/runtime.ts";
 import { ComponentChildren } from "https://esm.sh/v96/preact@10.11.0/src/index.d.ts";
 import { NavigationBar } from "./NavigationBar.tsx";
 
@@ -17,6 +17,7 @@ export function Layout({ pageTitle = "Fresh", children }: Props) {
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,600;1,300;1,600&display=swap"
           rel="stylesheet"
         />
+        <link rel="stylesheet" href={asset("style.css")} />
         <title>{pageTitle} | Dang that&apos;s Deno!</title>
       </Head>
       <main>
--- components/StoreCard.tsx
+++ components/StoreCard.tsx
@@ -4,11 +4,77 @@ type Props = {
 
 export function StoreCard({}: Props) {
   return (
-    <div class="bg-white ml-6 mt-6 shadow-md w-full sm:w-[45%] lg:w-[30%]">
-      {/* need to add before classes */}
-      <div class="relative px-6 text-right">
-        <div class="relative z-10 border-b-1 border-solid border-gray-50 mb-10 py-4 flex text-white items-center justify-around">
-          Comm
+    <div class="bg-white ml-6 mt-6 shadow-md w-full sm:w-[45%] lg:w-[30%] relative">
+      {/* need to add before classes above */}
+      <div class="relative px-6 text-right storeCardHero">
+        <div class="relative z-10 border-b-1 border-solid border-gray-50 mb-20 py-4 flex text-white items-center justify-around">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+              />
+            </svg>
+          </div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 3l1.664 1.664M21 21l-1.5-1.5m-5.485-1.242L12 17.25 4.5 21V8.742m.164-4.078a2.15 2.15 0 011.743-1.342 48.507 48.507 0 0111.186 0c1.1.128 1.907 1.077 1.907 2.185V19.5M4.664 4.664L19.5 19.5"
+              />
+            </svg>
+          </div>
+          <div class="p-1">
+            <a href="#stores/[slug]/edit">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </svg>
+            </a>
+          </div>
+          <div class="flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+              />
+            </svg>
+            <span class="ml-2">0</span>
+          </div>
         </div>
         <img
           src="https://media.istockphoto.com/id/1083487948/photo/tasty-pepperoni-pizza-and-cooking-ingredients-tomatoes-basil-on-black-concrete-background-top.jpg?s=612x612&w=0&k=20&c=E5rcp7F5y3SII4AYFmxFfcnkh6_LtVO4dzvjJDkXook="
@@ -16,10 +82,10 @@ export function StoreCard({}: Props) {
           class="h-full w-full top-0 right-0 absolute left-0 object-cover"
           style={{ clipPath: "polygon(0 0, 100% 0, 100% 90%, 0% 100%)" }}
         />
-        <h3 class="break-words relative m-0 text-4xl z-10 -skew-3 leading-5">
+        <h3 class="title">
           {/* add before classes */}
-          <a href="#storeId" class="border-none bg-storeCardTitle">
-            title
+          <a href="#stores/[slug]">
+            Pizza School
           </a>
         </h3>
       </div>
